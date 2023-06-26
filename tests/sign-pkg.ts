566f411db3eb2281fec87fd86deba94f2aa5e87d
Matt Didon <matt@onlx.co.uk>
2023-06-27 00:29:23 +0100

    Added test

---
+++ tests/sign-pkg.ts
@@ -0,0 +1,21 @@
+import { config }   from 'dotenv'
+import NotaryTool   from '../src/notarytool'
+
+// Call .env setup
+config()
+
+const main = async () => {
+
+    const tool = new NotaryTool({
+        appleApiKey         : process.env.APPLE_API_KEY,
+        appleApiKeyId       : process.env.APPLE_API_KEY_ID,
+        appleApiIssuerId    : process.env.APPLE_API_ISSUER_ID
+    })
+
+    tool.on('progress', val => console.log('progress:', val))
+    tool.on('status', (status, message) => console.log('status:', status, message))
+
+    await tool.notarize(process.env.FILE_PATH)
+
+}
+main()
